76b841c8f4c332ad95d2b7dfedc1b2148607b88b
koudai.ishida <ishida.0913@icloud.com>
2018-07-07 09:47:01 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-xs-offset-3 col-xs-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">REGISTER</div>
+            <div class="panel-body">
+                {!! Form::open(['route' => 'signup.post']) !!}
+                    <div class="form-group">
+                        {!! Form::label('name', 'NAME') !!}
+                        {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('email', 'E-MAIL') !!}
+                        {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password', 'PASSWORD') !!}
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('password_confirmation', 'PASSWORD（CONFIRMATION）') !!}
+                        {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                    </div>
+
+                    <div class="text-right">
+                        {!! Form::submit('REGISTER', ['class' => 'btn btn-success']) !!}
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
