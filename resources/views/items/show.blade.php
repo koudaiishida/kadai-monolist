89f06d6562077d280df3739fb4bf2c198242ba96
koudai.ishida <ishida.0913@icloud.com>
2018-07-08 14:52:17 +0000

    have

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -39,6 +39,9 @@
                         HAVE IT
                     </div>
                     <div class="panel-body">
+                        @foreach ($have_users as $user)
+                            <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
+                        @endforeach
                     </div>
                 </div>
             </div>
